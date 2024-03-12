a18904bca32dc98561acb60f539f07b4194fd62a
omarefj11 <omarbalmaceda97@gmail.com>
2024-03-11 22:15:30 -0500

    Correcciones

--- scripts.js
+++ scripts.js
@@ -15,3 +15,20 @@ function toggleMenu() {
   var body = document.body;
   body.classList.toggle("change-menu");
 }
+
+function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Mostrar el botón de ir arriba después de cierto desplazamiento
+window.addEventListener('scroll', function() {
+  var button = document.querySelector('.home');
+  if (window.scrollY > 50) { // Cambia 300 al número de píxeles después de los cuales deseas mostrar el botón
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
+});
